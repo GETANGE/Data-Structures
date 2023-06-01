8bdb148d15446b2066d8865736c27d13c97a8fa0
Getange <emmanuelgetange48@gmail.com>
2023-06-01 22:42:52 +0300

    ADD:bubblesort example2

--- bubblesort.js
+++ bubblesort.js
@@ -40,3 +40,62 @@ function bubbleSort(arr) {
 }
 bubbleSort(arr);
 console.log(arr);
+
+//EXAMPLE 2: ARRAY=[9,8,7,6,5,4,3,2,1]
+// ARRAY=[8,9,7,6,5,4,3,2,1]
+// ARRAY=[8,7,9,6,5,4,3,2,1]
+// ARRAY=[7,8,9,6,5,4,3,2,1]
+// ARRAY=[7,8,6,9,5,4,3,2,1]
+// ARRAY=[7,6,8,9,5,4,3,2,1]
+// ARRAY=[6,7,8,9,5,4,3,2,1]
+// ARRAY=[6,7,8,5,9,4,3,2,1]
+// ARRAY=[6,7,5,8,9,4,3,2,1]
+// ARRAY=[6,5,7,8,9,4,3,2,1]
+// ARRAY=[5,6,7,8,9,4,3,2,1]
+// ARRAY=[5,6,7,8,4,9,3,2,1]
+// ARRAY=[5,6,7,4,8,9,3,2,1]
+// ARRAY=[5,6,4,7,8,9,3,2,1]
+// ARRAY=[5,4,6,7,8,9,3,2,1]
+// ARRAY=[4,5,6,7,8,9,3,2,1]
+// ARRAY=[4,5,6,7,8,3,9,2,1]
+// ARRAY=[4,5,6,7,3,8,9,2,1]
+// ARRAY=[4,5,6,3,7,8,9,2,1]
+// ARRAY=[4,5,3,6,7,8,9,2,1]
+// ARRAY=[4,3,5,6,7,8,9,2,1]
+// ARRAY=[3,4,5,6,7,8,9,2,1]
+// ARRAY=[3,4,5,6,7,8,2,9,1]
+// ARRAY=[3,4,5,6,7,2,8,9,1]
+// ARRAY=[3,4,5,6,2,7,8,9,1]
+// ARRAY=[3,4,5,2,6,7,8,9,1]
+// ARRAY=[3,4,2,5,6,7,8,9,1]
+// ARRAY=[3,2,4,5,6,7,8,9,1]
+// ARRAY=[2,3,4,5,6,7,8,9,1]
+// ARRAY=[2,3,4,5,6,7,8,1,9]
+// ARRAY=[2,3,4,5,6,7,1,8,9]
+// ARRAY=[2,3,4,5,6,1,7,8,9]
+// ARRAY=[2,3,4,5,1,6,7,8,9]
+// ARRAY=[2,3,4,1,5,6,7,8,9]
+// ARRAY=[2,3,1,4,5,6,7,8,9]
+// ARRAY=[2,1,3,4,5,6,7,8,9]
+// ARRAY=[1,2,3,4,5,6,7,8,9]
+
+//lets code.
+let ARRAY=[9,8,7,6,5,4,3,2,1];
+function bubbleSort1(arr1){
+    let len1=arr1.length;
+    for(let i=0; i<len1; i++){
+        for(let k=0; k<len1; k++){
+            if(arr1[k]>arr1[k+1]){
+                let temp2=arr1[k];
+                arr1[k]=arr1[k+1];
+                arr1[k+1]=temp2;
+            }
+        }
+    }
+    return arr1;
+}
+console.log("The unsorted array is :" + ARRAY);
+
+//sorted array.
+let sortedArray =bubbleSort1(ARRAY);
+console.log(sortedArray);
