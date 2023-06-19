692fc6b2c2bf3d3216244ee662b06d28e8404c88
Getange <emmanuelgetange48@gmail.com>
2023-06-19 10:49:03 +0300

    ADD: more examples

--- searching.js
+++ searching.js
@@ -12,4 +12,38 @@ function search(s,x){
 let array=[2,5,6,7,9]
 var element=7;
 const index=search(array, element)
-console.log(index)
+console.log( element+" "+"is found at index"+" "+index)
+
+
+//This algorithm is based on a best case
+//This algorithm x is found in the array.
+function search2(s,x){
+    for(let i=0;i<s.length;i++){
+        if(s[i]==x){
+            return i;
+        }
+    }
+    return -1;
+}
+
+var arr = [2,5,7,9,10,11];
+const x = 10
+
+console.log( x+" is found at index "+ search2(arr,x) );
+
+//this algorithm is based on the worst case
+//This algorithm x1 is not found in the array.
+//When x1 is not present, the search3() function compares it with all the elements of arr[] one by one
+function search3(s,x){
+    for(let i=0;i<s.length;i++){
+        if(s[i]==x){
+            return i;
+        }
+    }
+    return -1;
+}
+
+var arr = [2,5,7,9,10,11];
+const x1= 12;
+
+console.log( x1+" is not found in the array and therefore we return"+" "+ search3(arr,x1) );
