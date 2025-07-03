67d7e1f4c0b009290c16378d527fe7feb893ac24
Getange <emmanuelgetange48@gmail.com>
2025-07-03 19:56:53 +0300

    BIG O: Quadratic time & Big O rules

--- performance.js
+++ performance.js
@@ -13,5 +13,19 @@ const findNemo = function(array){
 }
 findNemo(large) // O(n) -> It takes Linear time to find Tom Muli.
 
+// Constant Time complexity -> O(1)
 const arr = [10, 20, 30, 40];
-console.log(arr[2]); // Always takes the same time → O(1)
+console.log(arr[2]); // Always takes the same time → O(1) constants are ignored because Big O focuses on how the algorithm scales
+console.log(arr[0])
+
+// log all pairs of an array (Quadratic time -> )
+const boxes = [1, 2, 3, 4, 5];
+
+function logAllPairsOfArrays(arr){
+    for(let i=0; i<arr.length; i++){
+        for(let j=0; j<arr.length; j++){
+            console.log(arr[i], arr[j])
+        }
+    }
+}
+logAllPairsOfArrays(boxes)
